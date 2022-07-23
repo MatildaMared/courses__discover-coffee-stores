738f0a99371a78ba41dca2d1e2a5c5b168b88e33
Matilda Mared <matildamared@live.se>
2022-07-23 10:15:30 +0200

    add styles to banner component

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,6 @@ export default function Home() {
 			</Head>
 
 			<main className={styles.main}>
-				<h1 className={styles.title}>Coffee Connoisseur ☕️</h1>
 				<Banner handleClick={onBannerClick} />
 			</main>
 		</div>
